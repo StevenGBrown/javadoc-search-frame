2acedb28e9ab44d7fe9ee251f2072e16f2ed7890
StevenGBrown <StevenGBrown@users.noreply.github.com>
2009-11-01 23:15:25 +0900

    - Re-structured the directory layout again. Now the build scripts are kept together. - Implemented metadata.py, which interprets the SCRIPT_META_DATA variable for use within the build scripts. - The Greasemonkey build script now constructs the metadata block entirely from the SCRIPT_META_DATA variable. - Commenting and leading whitespace is no longer removed from the built script.

--- src/common/common.js
+++ src/common/common.js
@@ -1,53 +1,37 @@
-// ==UserScript==
-// @name          Javadoc Search Frame
-// @namespace     http://userscripts.org/users/46156
-// @description   Incremental search frame for Javadoc packages and classes.
-// @homepage      http://code.google.com/p/javadoc-search-frame
-// @version       DEVELOPMENT
-// @include       */allclasses-frame.html
-// @include       */package-frame.html
-// ==/UserScript==
-
-var SCRIPT_META_DATA = {
-  name : 'Javadoc Search Frame',
-  version : 'DEVELOPMENT',
-  homepage : 'http://code.google.com/p/javadoc-search-frame'
-};
-
 /**
-LICENSING
-
-This script is distributed under the MIT licence.
-http://en.wikipedia.org/wiki/MIT_License
-
-Copyright (c) 2009 Steven G. Brown
-
-The initial (24th February 2008) release was a fork of the Javadoc
-Incremental Search user script version 0.5 available at
-http://www.teria.com/~koseki/tools/gm/javadoc_isearch/index.html
-Copyright (c) 2006 KOSEKI Kengo
-
-Permission is hereby granted, free of charge, to any person
-obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without
-restriction, including without limitation the rights to use,
-copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following
-conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-*/
+ * JAVADOC SEARCH FRAME
+ *
+ * This script is distributed under the MIT licence.
+ * http://en.wikipedia.org/wiki/MIT_License
+ * 
+ * Copyright (c) 2009 Steven G. Brown
+ * 
+ * The initial (24th February 2008) release was a fork of the Javadoc
+ * Incremental Search user script version 0.5 available at
+ * http://www.teria.com/~koseki/tools/gm/javadoc_isearch/index.html
+ * Copyright (c) 2006 KOSEKI Kengo
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 
 (function () {
@@ -57,6 +41,18 @@ OTHER DEALINGS IN THE SOFTWARE.
  * GLOBAL VARIABLES
  * ----------------------------------------------------------------------------
  */
+
+/**
+ * Script metadata.
+ * 
+ * This variable is read by the build script.
+ */
+var SCRIPT_META_DATA = {
+  "name" : "Javadoc Search Frame",
+  "description" : "Incremental search frame for Javadoc packages and classes.",
+  "homepage" : "http://code.google.com/p/javadoc-search-frame",
+  "includes" : ["*/allclasses-frame.html", "*/package-frame.html"]
+};
 
 /**
  * Array of all package and class links.
