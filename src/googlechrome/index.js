dba37cc18bea445267b414401e0de7868b7d061b
StevenGBrown <StevenGBrown@users.noreply.github.com>
2009-11-08 12:58:17 +0800

    Google Chrome extension: packages frame is shown only when that setting has been configured, hidden otherwise.

--- src/googlechrome/index.js
+++ src/googlechrome/index.js
@@ -1,11 +1,42 @@
-// Hide the packages frame.
-var framesets = document.getElementsByTagName('frameset');
-if (framesets) {
-  var frameset = framesets[1];
-  if (frameset) {
-    frameset.setAttribute('rows', '0,*');
-    frameset.setAttribute('border', 0);
-    frameset.setAttribute('frameborder', 0);
-    frameset.setAttribute('framespacing', 0);
+/**
+ * This script is distributed under the MIT licence.
+ * http://en.wikipedia.org/wiki/MIT_License
+ * 
+ * Copyright (c) 2009 Steven G. Brown
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+if (localStorage.getItem('hide_package_frame') === 'true') {
+  // Hide the packages frame.
+  var framesets = document.getElementsByTagName('frameset');
+  if (framesets) {
+    var frameset = framesets[1];
+    if (frameset) {
+      frameset.setAttribute('rows', '0,*');
+      frameset.setAttribute('border', 0);
+      frameset.setAttribute('frameborder', 0);
+      frameset.setAttribute('framespacing', 0);
+    }
   }
 }
