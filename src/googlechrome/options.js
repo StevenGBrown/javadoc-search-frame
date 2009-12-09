92a17dc578f91b7667cdbbadec094db01c647706
StevenGBrown <StevenGBrown@localhost>
2009-12-10 00:03:45 +0800

    Added 'options.js'. This file was missing from r179.

---
+++ src/googlechrome/options.js
@@ -0,0 +1,61 @@
+/**
+ * JAVADOC SEARCH FRAME for Google Chrome.
+ * 
+ * This script is distributed under the MIT licence.
+ * http://en.wikipedia.org/wiki/MIT_License
+ * 
+ * Copyright (c) 2009 Steven G. Brown
+ * 
+ * The initial (24th February 2008) release was a fork of the Javadoc
+ * Incremental Search user script version 0.5 available at
+ * http://www.teria.com/~koseki/tools/gm/javadoc_isearch/index.html
+ * Copyright (c) 2006 KOSEKI Kengo
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+/*
+ * ----------------------------------------------------------------------------
+ * Storage
+ * ----------------------------------------------------------------------------
+ */
+
+#INCLUDE Storage.js
+
+
+/*
+ * ----------------------------------------------------------------------------
+ * Option
+ * ----------------------------------------------------------------------------
+ */
+
+#INCLUDE Option.js
+
+
+/*
+ * ----------------------------------------------------------------------------
+ * OptionsPageGenerator
+ * ----------------------------------------------------------------------------
+ */
+
+#INCLUDE OptionsPageGenerator.js
