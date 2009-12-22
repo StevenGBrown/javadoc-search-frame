5a66d037ca6f6fb0831ee8512e29b922b1828da3
StevenGBrown <StevenGBrown@localhost>
2009-12-22 15:37:47 +0800

    Updated the license headers of all build files.

--- build/buildlib/transformations.py
+++ build/buildlib/transformations.py
@@ -1,6 +1,5 @@
 """
-This script is distributed under the MIT licence.
-http://en.wikipedia.org/wiki/MIT_License
+The MIT License
 
 Copyright (c) 2009 Steven G. Brown
 
@@ -25,6 +24,7 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 """
+
 
 # Developed with Python v3.0.1
 
