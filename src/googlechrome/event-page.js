d9f220d23462f6908cdff8503e47533374546f49
Steven Brown <StevenGBrown@gmail.com>
2014-03-26 21:29:28 +0800

    Chrome: Migrate configured options from localStorage to storage.sync
    Preserve the configuration from earlier versions of the extension.
    
    With the new version, the configured options will be shared. So if a user has
    several Chrome browsers with different options, they may override each other.
    I have tried to minimise this by only sharing the non-default options.

--- src/googlechrome/event-page.js
+++ src/googlechrome/event-page.js
@@ -26,6 +26,48 @@
  */
 
 
+chrome.runtime.onInstalled.addListener(function(details) {
+  if (details.reason !== 'install' && details.reason !== 'update') {
+    return;
+  }
+  chrome.storage.local.get('optionsMigrated', function(items) {
+    if (items.optionsMigrated) {
+      return;
+    }
+    console.log('Migrating options from localStorage');
+    var itemsToSave = {};
+    var ver = '1.4.6';
+    var autoOpen = localStorage.getItem('auto_open');
+    if (autoOpen === 'true') {
+      itemsToSave['auto_open'] = {'value': true, 'version': ver};
+    }
+    var hidePackageFrame = localStorage.getItem('hide_package_frame');
+    if (hidePackageFrame === 'false') {
+      itemsToSave['hide_package_frame'] = {'value': false, 'version': ver};
+    }
+    var packageMenu = localStorage.getItem('package_menu');
+    var packageMenuDefault =
+        '@1:search(koders) -> http://www.koders.com/?s=##PACKAGE_NAME##\n' +
+        '@2:search(Docjar) -> http://www.docjar.com/s.jsp?q=##PACKAGE_NAME##';
+    if (packageMenu && packageMenu !== packageMenuDefault) {
+      itemsToSave['package_menu'] = {'value': packageMenu, 'version': ver};
+    }
+    var classMenu = localStorage.getItem('class_menu');
+    var classMenuDefault =
+        '@1:search(koders) -> http://www.koders.com/' +
+        '?s=##PACKAGE_NAME##+##CLASS_NAME##+##MEMBER_NAME##\n' +
+        '@2:search(Docjar) -> http://www.docjar.com/s.jsp?q=##CLASS_NAME##\n' +
+        '@3:source(Docjar) -> http://www.docjar.com/html/api/' +
+        '##PACKAGE_PATH##/##CLASS_NAME##.java.html';
+    if (classMenu && classMenu !== classMenuDefault) {
+      itemsToSave['class_menu'] = {'value': classMenu, 'version': ver};
+    }
+    chrome.storage.sync.set(itemsToSave);
+    console.log(itemsToSave);
+    chrome.storage.local.set({'optionsMigrated': true});
+  });
+});
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       var response = {};
