251f3d7c4924216c43175e9700f3bda6f44bbcba
Steven Brown <StevenGBrown@gmail.com>
2022-10-30 18:52:39 +0800

    Remove copyright notices from individual files

--- src/greasemonkey/javadoc_search_frame.js
+++ src/greasemonkey/javadoc_search_frame.js
@@ -16,38 +16,6 @@
 // @grant         GM_setValue
 // ==/UserScript==
 
-/**
- * The MIT License
- *
- * Copyright (c) 2008 Steven G. Brown
- *
- * The initial (24th February 2008) release was a fork of the Javadoc
- * Incremental Search user script version 0.5 available at
- * http://www.teria.com/~koseki/tools/gm/javadoc_isearch/index.html
- * Copyright (c) 2006 KOSEKI Kengo
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- */
-
 
 /**
  * Entry point of this script; called when the script has loaded.
