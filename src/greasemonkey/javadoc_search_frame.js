4e72c376ef5f1428dd289ecf722c0c8b5b9fc47c
Steven Brown <StevenGBrown@gmail.com>
2022-11-04 17:41:13 +0800

    Userscript: Add author and homepage fields for Tampermonkey

--- src/greasemonkey/javadoc_search_frame.js
+++ src/greasemonkey/javadoc_search_frame.js
@@ -2,6 +2,8 @@
 // @name          Javadoc Search Frame
 // @namespace     http://userscripts.org/users/46156
 // @description   Javadoc incremental search for packages and classes
+// @homepage      https://github.com/StevenGBrown/javadoc-search-frame
+// @author        Steven G. Brown
 // @copyright     2008, Steven G. Brown (https://github.com/StevenGBrown/javadoc-search-frame)
 // @copyright     2006, KOSEKI Kengo (http://www.teria.com/~koseki/tools/gm/javadoc_isearch/index.html)
 // @license       MIT License; http://www.opensource.org/licenses/mit-license.php
